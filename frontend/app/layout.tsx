616dbaaa2b54c612f3a34898d95b7d62ad54ef33
Viktor Luka <viktor.luka.dev@gmail.com>
2026-01-21 16:25:57 +0200

    Update frontend dependencies and styles; add  shadcn and other components for layout and notifications

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Header } from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Mini Time Tracker",
@@ -13,7 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={cn("bg-[#111111] antialiased")}>
+        <Header />
+        {children}
+        <Toaster
+          theme="dark"
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: "#18181b",
+              border: "1px solid #27272a",
+              color: "#ffffff",
+            },
+            className: "group",
+            classNames: {
+              success:
+                "bg-[#18181b] border-[#22c55e] text-white [&>svg]:text-[#22c55e]",
+              error:
+                "bg-[#18181b] border-red-500 text-white [&>svg]:text-red-500",
+              info: "bg-[#18181b] border-zinc-700 text-white",
+            },
+          }}
+        />
+      </body>
     </html>
   );
 }
